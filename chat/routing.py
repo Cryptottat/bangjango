f5b173e5316b76f65af0562e5540ff2d00364bb8
morgan-b-kim <morgan-b-kim@gmail.com>
2023-06-28 18:08:48 +0900

    new_version

--- chat/routing.py
+++ chat/routing.py
@@ -4,6 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<room_name>', consumers.ChatConsumer.as_asgi()),
-    path('ws/aa/', consumers.ChatConsumer),
-    path('ws/aa/a', consumers.ChatConsumer.as_asgi()),
+    path('ws/aa/', consumers.ChatConsumer.as_asgi()),
+    # path('ws/aa/a', consumers.ChatConsumer.as_asgi()),
 ]
