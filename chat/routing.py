ff0ac878589fb8b06e65514e49fee29ded1f7cfe
morgan-b-kim <morgan-b-kim@gmail.com>
2023-06-28 17:56:02 +0900

    new_version

--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<room_name>/', consumers.ChatConsumer),
+    path('ws/chat/<room_name>', consumers.ChatConsumer),
     # path('ws/main/', consumers.ChatConsumer),
 ]
