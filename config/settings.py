0acbb8e5eea5b1e1dc185eaa12f82f530d62f633
morgan-b-kim <morgan-b-kim@gmail.com>
2023-06-28 16:46:06 +0900

    settings.py - redis server setting changed

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6399)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
